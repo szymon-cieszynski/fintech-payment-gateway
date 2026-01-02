822b3f4b22d5a94e30eb4d379c63382ef87ee449
szymon-cieszynski <cieszynski8@gmail.com>
2026-01-02 18:55:14 +0100

    Create a lock when adding an account

--- src/Account/Application/AddAccount/AddAccountHandler.php
+++ src/Account/Application/AddAccount/AddAccountHandler.php
@@ -6,25 +6,36 @@ use App\Account\Domain\Account;
 use App\Account\Domain\AccountRepository;
 use App\Account\Domain\Currency;
 use App\Client\Domain\ClientRepository;
+use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 final readonly class AddAccountHandler
 {
     public function __construct(
         private AccountRepository $accountRepository,
-        private ClientRepository $clientRepository
+        private ClientRepository $clientRepository,
+        private LockFactory $accountCreationLockFactory
     ) {
     }
 
     #[AsMessageHandler]
     public function __invoke(AddAccountCommand $command): Account
     {
-        $currency = Currency::fromString($command->currency);
-        $client = $this->clientRepository->findByID($command->clientID);
+        $lock = $this->accountCreationLockFactory->createLock('account_creation' . $command->clientID, 5);
 
-        $account = Account::create($client, $currency);
-        $this->accountRepository->save($account);
+        try {
+            if (!$lock->acquire()) {
+                throw new \RuntimeException('Account creation already in progress.');
+            }
+            $currency = Currency::fromString($command->currency);
+            $client = $this->clientRepository->findByID($command->clientID);
+            $account = Account::create($client, $currency);
+            $this->accountRepository->save($account);
 
-        return $account;
+            return $account;
+        }
+        finally {
+                $lock->release();
+        }
     }
 }
--- tests/Account/Application/AddAccount/AddAccountHandlerTest.php
+++ tests/Account/Application/AddAccount/AddAccountHandlerTest.php
@@ -8,13 +8,19 @@ use App\Account\Domain\AccountRepository;
 use App\Client\Domain\Client;
 use App\Client\Domain\ClientRepository;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\LockInterface;
+use Symfony\Component\Lock\SharedLockInterface;
 
 class AddAccountHandlerTest extends TestCase
 {
-    public function testHandlerSavesClient(): void
+    public function testHandlerAddAccount(): void
     {
         $repoAccount = $this->createMock(AccountRepository::class);
         $repoClient = $this->createMock(ClientRepository::class);
+        $lockFactory = $this->createMock(LockFactory::class);
+        $lock = $this->createMock(SharedLockInterface::class);
+
         $repoAccount->expects($this->exactly(1))
              ->method('save');
 
@@ -26,7 +32,11 @@ class AddAccountHandlerTest extends TestCase
             ->with(99)
             ->willReturn($clientMock);
 
-        $handler = new AddAccountHandler($repoAccount, $repoClient);
+        $lockFactory->method('createLock')->willReturn($lock);
+        $lock->method('acquire')->willReturn(true);
+
+
+        $handler = new AddAccountHandler($repoAccount, $repoClient, $lockFactory);
 
         $cmd = new AddAccountCommand(99, 'PLN');
 
